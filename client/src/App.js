4138d1af831e14f266433c9d332fe1efcfe9a9f9
Robert Godlewski <robtechg5@gmail.com>
2022-06-15 16:24:27 -0700

    Found Pokedex API and made a table.

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,27 @@ import React, {useState} from 'react';
 
 // Components
 import NavBar from './components/NavBar';
+// Views all of the pokedexes in a table
+import ViewPokedex from './components/ViewPokedex';
+// Lists out all of the pokemon in a table
 import ViewPokemonList from './components/ViewPokemonList';
 
 
 function App() {
+  const [pokedexList, setPokedexList] = useState([]);
   const [pokemonList, setPokemonList] = useState([]);
 
   return (
     <div className="App">
       <NavBar/>
-      <ViewPokemonList pokemonList={pokemonList} setPokemonList={setPokemonList}/>
+      <ViewPokedex
+        pokedexList={pokedexList}
+        setPokedexList={setPokedexList}
+      />
+      <ViewPokemonList
+        pokemonList={pokemonList} 
+        setPokemonList={setPokemonList}
+      />
     </div>
   );
 };
--- client/src/components/ViewPokedex.js
+++ client/src/components/ViewPokedex.js
@@ -2,37 +2,37 @@ import React, {useEffect} from 'react';
 import axios from 'axios';
 
 
-const ViewPokemonList = (props) => {
-    const {pokemonList, setPokemonList} = props;
+const ViewPokedex = (props) => {
+    const {pokedexList, setPokedexList} = props;
 
     useEffect(() => {
-        axios.get("https://pokeapi.co/api/v2/pokemon/?limit=898")
+        axios.get("https://pokeapi.co/api/v2/pokedex/?limit=28")
         .then((res) => {
             //console.log(res);
             //console.log(res.data);
             console.log(res.data.results);
-            setPokemonList(res.data.results);
+            setPokedexList(res.data.results);
         })
         .catch((err) => {console.log(err)});
     }, []);
 
     return (
         <div>
-            <h3>All Pokemon</h3>
+            <h3>PokeDex List</h3>
             <table>
                 <thead>
                     <tr>
                         <th>ID Number</th>
-                        <th>Pokemon Name</th>
+                        <th>PokeDex Name</th>
                         <th>API Link</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {pokemonList.length > 0 && pokemonList.map((pokemon, index) => {
+                    {pokedexList.map((pokedex, index) => {
                         return (<tr key={index}>
                             <td>{index + 1}</td>
-                            <td>{pokemon.name}</td>
-                            <td>{pokemon.url}</td>
+                            <td>{pokedex.name}</td>
+                            <td>{pokedex.url}</td>
                         </tr>)
                     })}
                 </tbody>
@@ -49,4 +49,4 @@ const ViewPokemonList = (props) => {
 };
 
 
-export default ViewPokemonList;
+export default ViewPokedex;
